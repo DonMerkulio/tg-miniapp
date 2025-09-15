096b17a7b333cf58ed6afa4c6c6f270761587f28
DonMerkulio <user.aleksandrmerkylov@gmail.com>
2025-09-15 17:49:18 +0300

    изменение комментария для удаления резерва v4

--- app/routers/reserve_pages.py
+++ app/routers/reserve_pages.py
@@ -1,5 +1,7 @@
+# app/routers/reserve_pages.py
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from time import time
 from ..config import settings
 
 router = APIRouter()
@@ -7,8 +9,13 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/admin/reserves")
 def page_reserves(request: Request):
-    return templates.TemplateResponse("admin_reserves.html", {
-        "request": request,
-        "WEB_APP_URL": settings.web_app_url,
-        "ADMIN_USERNAME": settings.admin_username
-    })
+    return templates.TemplateResponse(
+        "admin_reserves.html",
+        {
+            "request": request,
+            "WEB_APP_URL": settings.web_app_url,
+            "ADMIN_USERNAME": settings.admin_username,
+            "ver": int(time()),              # ← версия для кэша
+        },
+        headers={"Cache-Control": "no-store"}  # ← страница не кэшируется
+    )
